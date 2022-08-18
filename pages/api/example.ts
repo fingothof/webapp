f161da0f91fb4a4560e6d6352e0148bc8d1e20f1
finneas <donteventry@backoff.gouv>
2022-08-18 07:22:49 -0400

    init2

---
+++ pages/api/example.ts
@@ -0,0 +1,13 @@
+// api example from the framework. To be removed 
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: 'John Doe' })
+}
