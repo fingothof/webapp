9a2d99bf1e4fee88e59fbcf0be072d5a16c47e43
obrian33 <obrians@uci.edu>
2022-08-26 22:04:54 -0700

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-		<div>
-			<a href="/calendar"> calendar </a>
-    </div>
+		<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
