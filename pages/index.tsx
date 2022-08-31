a26a5bf615adc5ec7fa8ae831547dd588693bf1e
obrian33 <obrians@uci.edu>
2022-08-30 19:37:09 -0700

    added initial Menu and Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next'
+import { Bars3Icon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
-		<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='flex flex-col items-center mx-auto h-screen'>
+      <div className='flex h-24 justify-center items-center'>
+        <Bars3Icon className='w-12 p-2'></Bars3Icon>
+        <div className=" text-3xl font-bold">
+          Upcoming Events!
+        </div>
+      </div>
+    </div>
   )
 }
 
