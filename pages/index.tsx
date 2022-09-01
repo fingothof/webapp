8f2d5b09003a06a95168e5112057ab453b84549c
obrian33 <obrians@uci.edu>
2022-08-31 17:04:42 -0700

    Added background colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className='flex flex-col items-center mx-auto h-screen'>
-      <div className='flex h-24 justify-center items-center'>
-        <Bars3Icon className='w-12 p-2'></Bars3Icon>
-        <div className=" text-3xl font-bold">
+    <div className='flex flex-col items-center mx-auto h-screen bg-brand-100'>
+      <div className='flex h-24 w-screen justify-start items-center'>
+        <Bars3Icon className='w-12 p-2 text-brand-500'></Bars3Icon>
+        <div className="flex justify-center w-screen text-brand-500 text-3xl font-bold">
           Upcoming Events!
         </div>
       </div>
