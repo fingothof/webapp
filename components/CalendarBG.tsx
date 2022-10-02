f6fbaf0a3713ef43055820297991110b1d4337b2
finneas <donteventry@backoff.gouv>
2022-10-02 12:57:10 -0400

    stashing or something whatever

--- components/CalendarBG.tsx
+++ components/CalendarBG.tsx
@@ -1,21 +1,19 @@
 import * as React from "react";
 import Month from "./Month";
 import {ReactNode, useEffect,useState} from 'react'
+import { stat } from "fs";
 
 //TEMP VARS
 let url = "http://localhost:3000"
-let year = 2022
 
 //TYPES
 type Data = Map<Object, Array<Array<string>>>
 
 //VARS
-let today = new Date()
 let bounds = {
     min: { year:2022, month:0},
     max: { year:2024, month:11}
 }
-
 
 //Creates the days and months
 let setData = (data:any) : Data => {
@@ -29,23 +27,13 @@ let setData = (data:any) : Data => {
                 //Keep in mind that months are 0-11 in code and 1-12 in db data
                 let tag = year+'.'+(i+1).toString()+'.'+(j+1).toString()
                 data[tag] ? currArr.push(data[tag]):
-                            currArr.push(["empty"])
+                            currArr.push([])
             }
             calData.set(year.toString() + i.toString(),currArr)
         }
     }
-    console.log(calData)
     return calData
 }
-
-let fetchedData: Data | null = null
-
-let fetchData = (async () => {
-    let res = await fetch(url + '/api/events')
-    let data = await res.json()
-    fetchedData = setData(data)
-    return setData(data)
-})().then((val) => val)
 
 //seems redundant
 let createMonthsComp = (year:number, month:number, data: Array<string>|undefined) : ReactNode =>{
@@ -61,25 +49,43 @@ let formatMonth = (month: number) => {
     return months[month%12]
 }
 
+let setupData = async (stateUpdate: Function) => {
+    let res = await fetch(url + '/api/events')
+    let data = await res.json()
+    let dateData = setData(data)
+    let today = new Date()
+    let month = (bounds.min.year - (today.getFullYear())) * 12 + (today.getMonth())
+    setupMonth(dateData, month, stateUpdate)
+}
+
+let setupMonth = (data: Data, month: number, stateUpdate: Function) => {
+    let year = (bounds.min.year + Math.floor(month/12))
+    let currentMonth = (month%12)
+    let currentData = data.get(year.toString()+currentMonth.toString())
+    let component = createMonthsComp(year,currentMonth, currentData)
+    console.log(component)
+    stateUpdate(component)
+}
+
+let dataStatus = () => {
+    
+}
+
 let CalendarBG = () =>{
     let [month,setMonth] = useState(-1) 
     let [component,setComponent] = useState<ReactNode>(<div/>) 
+    let [test,setTest] = useState(0)
 
-    useEffect(() => {
-        if(month == -1){
-            setMonth(today.getMonth())
+    useEffect(() =>{
+        if(!test){
+            setTest(1)
+            setupData(setComponent)
         }
-        //dubious, without checking for month, this is infinitely called
-        //temp data for month <=>
-        if(fetchedData && month >= 0){
-            let year = (2022 + Math.floor(month/12))
-            let currentMonth = (month%12)
-            let currentData = fetchedData.get(year.toString()+currentMonth.toString())
-            let component = createMonthsComp(year,currentMonth, currentData)
-            setComponent(component)
+        else{
+            console.log("wtf")
         }
-    },[month])
-
+    })
+    
     //min and max months to be displayed
     let upperBound = ((bounds.max.year - bounds.min.year) * 12 + bounds.max.month)
     let lowerBound = 0
@@ -89,7 +95,7 @@ let CalendarBG = () =>{
             <button onClick={() => setMonth(month-1)} > 
                 {month > lowerBound ? <p> &lt; </p> : <p></p>}
             </button>
-            { year + Math.floor(month/12) }  {formatMonth(month)} 
+            { bounds.min.year + Math.floor(month/12) }  {formatMonth(month)} 
             <button onClick={() => setMonth(month+1)} > 
                 {month < (upperBound) ? <p> &gt; </p> : <p></p>}
             </button>
