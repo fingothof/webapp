18ee59ae96a3c2e82da795b8b93495634e38486d
obrian33 <obrians@uci.edu>
2022-10-02 17:12:25 -0700

    Added mysql connector and serverside props

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,12 +1,41 @@
 import type { NextPage } from 'next'
+import { AppContext } from 'next/app';
 import Month from '../components/Month'
+import { GetServerSideProps } from 'next';
+import mysql from 'mysql2';
 
-const Calendar: NextPage = () => {
+interface Props {
+  userAgent?: string;
+}
+
+const Calendar: NextPage<Props> = ({userAgent}) => {
+  
   return (
 		<div>
-			<Month days={31}/>
+      <div>{userAgent}</div>
+			<Month days={31}></Month>
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  var connection = mysql.createConnection({
+    connectionLimit: 4,
+    host     : process.env.DB_HOST,
+    user     : process.env.MYSQLDB_USER,
+    password : process.env.MYSQLDB_ROOT_PASSWORD,
+    database : process.env.MYSQLDB_DATABASE
+  });
+
+  let [rows] = await connection.promise().query('SELECT * FROM Users');
+  let temp: any = rows;
+  
+  return {
+    props: {
+      userAgent: temp[0].Username
+    }
+
+  };
+} 
 
 export default Calendar
