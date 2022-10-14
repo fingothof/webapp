f2438181f1bdd3de167606113e061f917e4d2a69
obrian33 <obrians@uci.edu>
2022-10-13 21:06:11 -0700

    FINALLY connected to redis correctly

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -3,6 +3,7 @@ import { AppContext } from 'next/app';
 import Month from '../components/Month'
 import { GetServerSideProps } from 'next';
 import mysql from 'mysql2';
+import Redis from 'ioredis';
 
 interface Props {
   userAgent?: string;
@@ -28,7 +29,15 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   });
 
   // send cookie id to redis.
+  console.log('starting redis');
+  let redis = new Redis({
+    host: 'redis-store',
+    password: process.env.REDIS_PASSWORD
+  });
   // If present and session is valid return username.
+  let res = await redis.get('key1');
+  console.log(res);
+  console.log('test redis');
   let [rows] = await connection.promise().query('SELECT * FROM Users');
   let temp: any = rows;
   
